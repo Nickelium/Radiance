a3a39dda04e395ba0b8f8b597191cc789c5c1de0
Nickelium <trantr.be@gmail.com>
2019-06-13 23:29:12 +0200

    Initial Project & Solution Setup

---
+++ Radiance/src/Test.cpp
@@ -0,0 +1,9 @@
+#include "Test.h"
+#include <stdio.h>
+namespace Radiance
+{
+	void Print()
+	{
+		printf("Welcome to Radiance Engine\n");
+	}
+}
---
+++ Sandbox/src/Application.cpp
@@ -0,0 +1,11 @@
+
+namespace Radiance
+{
+	__declspec(dllimport) void Print();
+}
+
+int main(int argc, char** argv)
+{
+	Radiance::Print();
+	return 0;
+}
