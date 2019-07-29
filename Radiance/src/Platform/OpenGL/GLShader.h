c322ae6eb499aae87f74e5493fdfac07f08fb53f
Nickelium <trantr.be@gmail.com>
2019-07-29 19:22:44 +0200

    Refactor Shader easier but less verbose API

--- Radiance/src/Platform/OpenGL/GLShader.h
+++ Radiance/src/Platform/OpenGL/GLShader.h
@@ -22,22 +22,23 @@ namespace Radiance
 		virtual void Bind() const override;
 		virtual void UnBind() const override;
 
-		virtual void SetUniformFloat(const std::string& _name, float _val) override;
-		virtual void SetUniformFloats(const std::string& _name, const std::vector<float>& _val) override;
-		virtual void SetUniformInt(const std::string& _namee, int _value) override;
-		virtual void SetUniformInts(const std::string& _name, const std::vector<int>& _val) override;
-		virtual void SetUniformFloat2(const std::string& _name, const glm::vec2& _val) override;
-		virtual void SetUniformFloat3(const std::string& _name, const glm::vec3& _val) override;
-		virtual void SetUniformFloat4(const std::string& _name, const glm::vec4& _val) override;
-		virtual void SetUniformMat(const std::string& _name, const glm::mat4& _val) override;
+		virtual void SetUniform(const std::string& _name, bool _val);
+		virtual void SetUniform(const std::string& _name, float _val) override;
+		virtual void SetUniform(const std::string& _name, const std::vector<float>& _val) override;
+		virtual void SetUniform(const std::string& _namee, int _value) override;
+		virtual void SetUniform(const std::string& _name, const std::vector<int>& _val) override;
+		virtual void SetUniform(const std::string& _name, const glm::vec2& _val) override;
+		virtual void SetUniform(const std::string& _name, const glm::vec3& _val) override;
+		virtual void SetUniform(const std::string& _name, const glm::vec4& _val) override;
+		virtual void SetUniform(const std::string& _name, const glm::mat4& _val) override;
 
 	private:
 		Handle CreateShader(const std::string& _source, ShaderType _type);
 		Handle CreateProgram(unsigned int _vertexID, unsigned int _fragmentID);
 
-		Location GetLocation(const std::string& _name);
+		Location GetLocation(const std::string& _name) const;
 		
-		MapLocation m_MapLocation;
+		mutable MapLocation m_MapLocation;
 		Handle m_Handle;
 
 		static const Location s_InvalidLocation = -1;
