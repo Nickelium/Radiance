1c50e931896ebd0400f515622a39e7d34a295a97
Nickelium <trantr.be@gmail.com>
2019-08-04 17:48:46 +0200

    Changed to CoreContext

--- Radiance/src/Platform/Windows/WindowsWindow.cpp
+++ Radiance/src/Platform/Windows/WindowsWindow.cpp
@@ -59,6 +59,11 @@ namespace Radiance
 		glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
 		#endif
 
+		//GLCallback requires GL4.3+
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
 		m_Window = glfwCreateWindow((int)_desc.Width, (int)_desc.Height, m_Data.Title.c_str(), nullptr, nullptr);
 		RAD_CORE_ASSERT(m_Window, "Window is nullptr");
 
